c5f38f462201b8999634f23a678d767c19c3f44d
Syam <hellnotnow@protonmail.com>
2021-08-16 13:57:44 +0530

    Update Pylookup.py

--- Pylookup.py
+++ Pylookup.py
@@ -3,7 +3,6 @@ import os
 from time import sleep
 import alonsercher
 import socodex
-import linkes
 import crawler
 
 
@@ -28,7 +27,7 @@ while(askusr == 'y' or askusr == 'Y'):
    os.system("cat resources/logo.txt")#changes to be made
    os.system("cat resources/devinfo.txt")#changes to be made
    
-   print("\n\n\t0 to exit \n\t1 search username on  all prebuild templates \n\t2 search username from a specific from prebuild template \n\t3 source code extractor  \n\t5 Web scrapping ")
+   print("\n\n\t0 to exit \n\t1 search username on  all prebuild templates \n\t2 search username from a specific from prebuild template \n\t3 source code extractor  \n\t4 Web scrapping ")
    m=int(input("\n enter your choice \u279c "))
    if(m == 1):
        osinter.mainer()
@@ -36,11 +35,9 @@ while(askusr == 'y' or askusr == 'Y'):
       alonsercher.alon()
    elif(m == 3):
       socodex.sorc()
-   elif(m == 4):
-      linkes.act()
    elif(m==0):
       exit(0)
-   elif(m == 5):
+   elif(m == 4):
       crawler.web()
 
    
