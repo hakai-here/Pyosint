a69178f4283991f038f9a6132f3b75af268597c4
Syam <hellnotnow@protonmail.com>
2021-08-20 13:01:09 +0530

    Update subdomenum.py

--- subdomenum.py
+++ subdomenum.py
@@ -53,7 +53,7 @@ if is_windows:
         colorama.init()
         #Now the unicode will work ^_^
     except:
-        print("[!] Error: Coloring libraries not installed, no coloring will be used [Check the readme]")
+        print(" Error: Coloring libraries not installed, no coloring will be used ")
         G = Y = B = R = W = G = Y = B = R = W = ''
 
 
