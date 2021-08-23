59eca06b0666a218b3a9776e9652f4bb70fc543e
Syam <hellnotnow@protonmail.com>
2021-08-23 17:46:40 +0530

    Update Pyosint.py

--- Pyosint.py
+++ Pyosint.py
@@ -45,8 +45,8 @@ def run_find(j):
         try:
             response=requests.get(url)
             if (response.status_code == 200):
-                print("╬➤ "+ url)
-                print("╬")
+                print(" ╬➤ "+ url)
+                print(" ╬")
             else:
                 pass
         except ConnectionError:
